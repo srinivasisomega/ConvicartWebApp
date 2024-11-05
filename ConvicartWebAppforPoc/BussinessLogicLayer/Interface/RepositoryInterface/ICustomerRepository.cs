a17b015dc9a12fdb96773167b5bd2bb4d10b4a57
srinivas <srinivas.pallamparthi@cognine.com>
2024-11-05 10:23:40 +0530

    Add project files.

---
+++ ConvicartWebAppforPoc/BussinessLogicLayer/Interface/RepositoryInterface/ICustomerRepository.cs
@@ -0,0 +1,13 @@
+﻿using ConvicartWebApp.DataAccessLayer.Models;
+
+namespace ConvicartWebApp.BussinessLogicLayer.Interface.RepositoryInterface
+{
+    public interface ICustomerRepository : IRepository<Customer>
+    {
+        Task<Customer?> GetCustomerWithAddressByIdAsync(int customerId);
+        Task<Customer?> GetCustomerByEmailAsync(string email);
+        Task<Customer?> GetCustomerByEmailAndPasswordAsync(string email, string password);
+    }
+
+
+}
